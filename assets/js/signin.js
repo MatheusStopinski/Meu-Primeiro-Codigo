0d24fe1ab8df42461d9f08f27a144e99c81acb94
Matheus Stopinski <matheus.stopinski@hotmail.com>
2024-01-22 01:59:57 -0300

    1º código ja criado na vida!

--- assets/js/signin.js
+++ assets/js/signin.js
@@ -1,116 +1,111 @@
-const bluePill = document.getElementById("pill-1");
-const redPill = document.getElementById("pill-2");
-const videoContainer = document.getElementById("video-container");
-const video = document.getElementById("my-video");
-const greeting = document.getElementById("greeting");
-const msg1Container = document.getElementById("container-msg1");
-const msg2Container = document.getElementById("container-msg2");
-const msg3Container = document.getElementById("container-msg3");
-const msg4Container = document.getElementById("container-msg4");
-const msg1 = document.getElementById("msg1");
-const msg2 = document.getElementById("msg2");
-const msg3 = document.getElementById("msg3");
-const msg4 = document.getElementById("msg4");
-const loginBtn = document.getElementById("login-btn");
+let btn = document.querySelector(".fa-eye");
+btn.addEventListener("click", () => {
+  let inputPassword = document.querySelector("#Password");
 
-document.addEventListener("DOMContentLoaded", function () {
-  
-  bluePill.addEventListener("click", function () {
-    window.location.href = "https://www.instagram.com/matheus_stopinski/";
-  });
-
-  redPill.addEventListener("click", function () {
-    videoContainer.style.display = "block";
-    video.src =
-      "https://drive.google.com/uc?export=download&id=1YbUmJjPXCDFPzj0z7azbpnIbf1ZJz45m";
-    video.load();
-    video.play();
-    bluePill.style.display = "none"; // Oculta
-    redPill.style.display = "none"; // Oculta
-  });
-
-  video.addEventListener("loadedmetadata", function () {
-    const videoWidth = video.videoWidth;
-    const videoHeight = video.videoHeight;
-    const videoContainerWidth = videoContainer.offsetWidth;
-    const videoContainerHeight = videoContainer.offsetHeight;
-    const containerRatio = videoContainerWidth / videoContainerHeight;
-    const videoRatio = videoWidth / videoHeight;
-
-    if (containerRatio > videoRatio) {
-      video.style.width = "100%";
-      video.style.height = "auto";
-    } else {
-      video.style.width = "auto";
-      video.style.height = "100%";
-    }
-  });
-
-  video.addEventListener("canplay", function () {
-    video.play();
-  });
-
-  video.addEventListener("timeupdate", function () {
-    if (video.currentTime >= -4 && video.currentTime <= 0) {
-      greeting.style.visibility = "visible";
-    } else {
-      greeting.style.visibility = "hidden";
-    }
-  });
-
-  video.addEventListener("timeupdate", function () {
-    if (video.currentTime >= 11.1 && video.currentTime <= 26) {
-      msg1Container.style.display = "block";
-      msg1.style.visibility = "visible";
-    } else {
-      msg1Container.style.display = "none";
-      msg1.style.visibility = "hidden";
-    }
-  });
-
-  video.addEventListener("timeupdate", function () {
-    if (video.currentTime >= 26.7 && video.currentTime <= 40.5) {
-      msg2Container.style.display = "block";
-      msg2.style.visibility = "visible";
-    } else {
-      msg2Container.style.display = "none";
-      msg2.style.visibility = "hidden";
-    }
-  });
-
-  video.addEventListener("timeupdate", function () {
-    if (video.currentTime >= 41.5 && video.currentTime <= 49.7) {
-      msg3Container.style.display = "block";
-      msg3.style.visibility = "visible";
-    } else {
-      msg3Container.style.display = "none";
-      msg3.style.visibility = "hidden";
-    }
-  });
-
-  video.addEventListener("timeupdate", function () {
-    if (video.currentTime >= 81.6 && video.currentTime <= 93.9) {
-      msg4Container.style.display = "block";
-      msg4.style.visibility = "visible";
-    } else {
-      msg4Container.style.display = "none";
-      msg4.style.visibility = "hidden";
-    }
-  });
-
-  video.addEventListener("ended", function () {
-    loginBtn.style.display = "block";
-  });
-
-  loginBtn.addEventListener("click", function () {
-    window.location.href =
-      "";
-  });
+  if (inputPassword.getAttribute("type") == "password") {
+    inputPassword.setAttribute("type", "text");
+  } else {
+    inputPassword.setAttribute("type", "password");
+  }
 });
 
-/* NOTA: Por ser minha primeira vez, 
-notei que quero sempre fazer algo a mais, só que isso não tem fim. 
- Então mesmo não querendo, 
- decedi finalizar por aqui, para poder dar inicio  a outros projetos!
- 13/09/2023 Parabéns "My Good" to orgulhoso de vc, 
- desejo que seu trajeto seja cada vez mais criativo, afinal isso te move. */
+function enter() {
+  let usuario = document.querySelector("#usuario");
+  let userLabel = document.querySelector("#userLabel");
+  let Password = document.querySelector("#Password");
+  let PasswordLabel = document.querySelector("#PasswordLabel");
+
+  let msgError = document.querySelector("#msgError");
+  let listaUser = [];
+
+  let userValid = {
+    name: "",
+    Password: ""
+  };
+
+  listaUser = JSON.parse(localStorage.getItem("listaUser"));
+
+  listaUser.forEach((item) => {
+    if (usuario.value == item.nameCad && Password.value == item.passwordCad) {
+      userValid = {
+        name: item.nameCad,
+        Password: item.passwordCad,
+      };
+    }
+  });
+
+  if (usuario.value === "" && Password.value === "") {
+    resetUserField();
+    resetPasswordField();
+  } else if (usuario.value === "" || Password.value === "") {
+    if (usuario.value === "") {
+      resetUserField();
+    } else {
+      resetPasswordField();
+    }
+  } else if (
+    usuario.value == userValid.name &&
+    Password.value == userValid.Password
+  ) {
+    window.location.href = '../assets/html/index.html'
+
+    let token =
+      Math.random().toString(16).substr(2) +
+      Math.random().toString(16).substr(2);
+
+    localStorage.setItem("token", token); /* localStorage só aceita string */
+    localStorage.setItem("userLogado", JSON.stringify(userValid));
+  } else {
+    userLabel.setAttribute("style", "color: #ce0000");
+    userLabel.innerHTML =
+      '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">User</strong>';
+    usuario.setAttribute("style", "border-color: red");
+
+    PasswordLabel.setAttribute("style", "color: #ce0000");
+    PasswordLabel.innerHTML =
+      '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">Password</strong>';
+    Password.setAttribute("style", "border-color: red");
+    msgError.setAttribute("style", "display: block");
+    msgError.innerHTML =
+      "<strong style='font-weight: 570; text-shadow: 0 0 8px rgba(9, 13, 34, 0.2);'>User or Password are incorrect</strong>";
+    msgError.classList.add("error-message");
+    msgError.style.display = "block";
+    usuario.focus();
+  }
+}
+
+let usuario = document.querySelector("#usuario");
+let Password = document.querySelector("#Password");
+let userLabel = document.querySelector("#userLabel");
+let PasswordLabel = document.querySelector("#PasswordLabel");
+let msgError = document.querySelector("#msgError");
+
+// Manipulador de evento para o campo de usuário
+usuario.addEventListener("input", () => {
+  if (usuario.value === "") {
+    resetUserField();
+  }
+});
+
+// Manipulador de evento para o campo de senha
+Password.addEventListener("input", () => {
+  if (Password.value === "") {
+    resetPasswordField();
+  }
+});
+
+// Função para redefinir o campo de usuário
+function resetUserField() {
+  userLabel.removeAttribute("style");
+  usuario.removeAttribute("style");
+  msgError.removeAttribute("style");
+  userLabel.innerHTML = "User";
+}
+
+// Função para redefinir o campo de senha
+function resetPasswordField() {
+  PasswordLabel.removeAttribute("style");
+  Password.removeAttribute("style");
+  msgError.removeAttribute("style");
+  PasswordLabel.innerHTML = "Password";
+}
---
+++ assets/js/signup.js
@@ -0,0 +1,167 @@
+
+let name = document.querySelector("#name");
+let Labelname = document.querySelector("#Labelname");
+let validname = false;
+
+let Email = document.querySelector("#Email");
+let LabelEmail = document.querySelector("#LabelEmail");
+let validEmail = false;
+
+let Password = document.querySelector("#Password");
+let LabelPassword = document.querySelector("#LabelPassword");
+let validPassword = false;
+
+let ConfirmPassword = document.querySelector("#ConfirmPassword");
+let LabelConfirmPassword = document.querySelector("#LabelConfirmPassword");
+let validConfirmPassword = false;
+
+let msgSuccess = document.querySelector('#msgSuccess')
+let msgError = document.querySelector('#msgError')
+
+let btn = document.querySelector("#SeePassword");
+let btnConfirm = document.querySelector("#SeeConfirmPassword");
+
+/* --------------------------------------- CAMPO DE FORMULÁRIO ------------------------------------- */
+
+name.addEventListener("keyup", () => {
+  if (name.value.length >= 1 && name.value.length <= 2) {
+    Labelname.setAttribute('style', 'color: red');
+    name.setAttribute('style', 'border-color: red');
+    Labelname.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">Name</strong>';
+    validname = false;
+  } else if (name.value.length >= 3) {
+    Labelname.setAttribute('style', 'color: #1bd61b');
+    name.setAttribute('style', 'border-color: #1bd61b');
+    Labelname.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">Name</strong>';
+    validname = true;
+    
+  } else {
+    Labelname.removeAttribute('style');
+    name.removeAttribute('style');
+    Labelname.innerHTML = 'Name';
+    validname = true;
+  }
+});
+
+Email.addEventListener('keyup', () => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expressão regular para validar o formato do e-mail
+
+  if (Email.value === '') {
+    LabelEmail.removeAttribute('style');
+    Email.removeAttribute('style');
+    LabelEmail.innerHTML = 'E-mail';
+    validEmail = true;
+  } else if (Email.value !== '' && emailRegex.test(Email.value)) {
+    LabelEmail.setAttribute('style', 'color: #1bd61b');
+    LabelEmail.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">E-mail</strong>';
+    Email.setAttribute('style', 'border-color: #1bd61b');
+    validEmail = true;
+  } else {
+    LabelEmail.setAttribute('style', 'color: red');
+    LabelEmail.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">E-mail</strong>';
+    Email.setAttribute('style', 'border-color: red');
+    validEmail = false;
+  }
+});
+
+Password.addEventListener('keyup', () => {
+  if (Password.value.length >= 1 && Password.value.length <= 6) {
+    LabelPassword.setAttribute('style', 'color: red');
+    LabelPassword.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">Password *More than 7 characters</strong>';
+    Password.setAttribute('style', 'border-color: red');
+    validPassword = false;
+  } else if (Password.value.length > 6) {
+    LabelPassword.setAttribute('style', 'color: #1bd61b');
+    LabelPassword.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgba(40, 40, 40, 9.0);">Password</strong>';
+    Password.setAttribute('style', 'border-color: #1bd61b');
+    validPassword = true;
+  } else {
+    LabelPassword.removeAttribute('style');
+    Password.removeAttribute('style');
+    LabelPassword.innerHTML = 'Password';
+    validPassword = true;
+  }
+});
+
+ConfirmPassword.addEventListener('keyup', () => {
+  if (ConfirmPassword.value.length >= 1 && ConfirmPassword.value !== '') {
+    if (ConfirmPassword.value !== Password.value) {
+      LabelConfirmPassword.setAttribute('style', 'color: red');
+      LabelConfirmPassword.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgb(40, 40, 40,  9.0);">Confirm Password</strong>';
+      ConfirmPassword.setAttribute('style', 'border-color: red');
+      validConfirmPassword = false;
+    } else {
+      LabelConfirmPassword.setAttribute('style', 'color: #1bd61b');
+      LabelConfirmPassword.innerHTML = '<strong style="font-weight: 480; text-shadow: 0 0 8px rgb(40, 40, 40,  9.0);">Confirm Password</strong>';
+      ConfirmPassword.setAttribute('style', 'border-color: #1bd61b');
+      validConfirmPassword = true;
+    }
+  } else {
+    LabelConfirmPassword.removeAttribute('style');
+    ConfirmPassword.removeAttribute('style');
+    LabelConfirmPassword.innerHTML = 'Confirm Password';
+    validConfirmPassword = true;
+  }
+});
+
+/* -------------------------------------- FROM HTML TO JAVASCRIPT ---------------------------------------- */
+
+function Register() {
+  if (validname && validEmail && validPassword && validConfirmPassword) {
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
+    
+    listaUser.push({
+      nameCad: name.value,
+      emailCad: Email.value,
+      passwordCad: Password.value,
+    });
+    
+    
+    localStorage.setItem('listaUser', JSON.stringify(listaUser));
+    
+       
+    
+    msgSuccess.setAttribute('style', 'display: block');
+    msgSuccess.innerHTML = '<strong> Sucesso! Redirecioando para o login....</strong>';
+    msgError.setAttribute('style', 'display: none');
+    msgError.innerHTML = '';
+    
+
+    
+    setTimeout(() => {
+    /* troca a localização da minha pagina */
+    window.location.href = '../../signin.html';
+    },2803) /* delay de quase 3 segundos */
+    
+     
+  
+  } else {
+    msgError.setAttribute('style', 'display: block');
+    msgError.innerHTML = '<strong>Complete all the fields correctly</strong>';
+    msgSuccess.innerHTML = '';
+    msgSuccess.setAttribute('style', 'display: none');
+        
+  }
+}
+    
+
+/* ----------------------------- BTN'S = Botão do olho ------------------------------------- */
+
+btn.addEventListener("click", () => {
+ let inputPassword = document.querySelector("#Password");
+ if (inputPassword.getAttribute("type") == "password") {
+    inputPassword.setAttribute("type", "text");
+} else {
+    inputPassword.setAttribute("type", "password");
+  }
+ });
+  
+btnConfirm.addEventListener("click", () => {
+  let inputConfirmPassword = document.querySelector("#ConfirmPassword")
+  if (inputConfirmPassword.getAttribute("type") == "password") {
+    inputConfirmPassword.setAttribute("type", "text")
+  } else {
+    inputConfirmPassword.setAttribute("type", "password")
+  }
+});
+  
